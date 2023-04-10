df34b4424b422eeb1f13c6e19cba6943f83b4b05
Omar Zeineddine <ozeineddine.dev@gmail.com>
2023-04-10 12:43:31 +0300

    feat - layout and fonts

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About next-13",
+};
+
 const AboutPage = () => {
   return (
     <div>
--- app/about/team/page.jsx
+++ app/about/team/page.jsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About team page",
+};
+
 const TeamPage = () => {
   return (
     <div>
